87aba6f8df9e1c8d9e0c18a124962c186071fcb9
JohnCanessa <john.canessa@gmail.com>
2022-06-30 15:39:48 -0500

    updated main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+*/
+int main ()
+{
+  
+  // **** display message ****
+  cout << "Hello John !!!";
+  cout << endl;
+  
+  // **** all is well ****
+  return 0;
+}
