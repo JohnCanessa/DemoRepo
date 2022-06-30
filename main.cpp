9d2af79a76ef12017bc2a153f49f5993810b8914
JohnCanessa <john.canessa@gmail.com>
2022-06-30 15:48:15 -0500

    two lines

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+*/
+int main ()
+{
+  
+  // **** display two line message ****
+  cout << "Hello";
+  cout << " John !!!" << endl;
+  
+  // **** all is well ****
+  return 0;
+}
